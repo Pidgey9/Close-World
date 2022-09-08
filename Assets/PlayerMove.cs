3d6c749d141f5ee42f4f9ad8e11cebd9d7eac608
Pidgey <bastien.bouchet@3wa.io>
2022-09-08 14:10:12 +0200

    Light, save us all

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rb;
+    Vector2 move;
+    public float speed;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        move = new Vector2(h, v);
+    }
     private void FixedUpdate()
     {
-        rb.AddTorque(1);
+        rb.velocity = move * speed * Time.deltaTime;
     }
 }
