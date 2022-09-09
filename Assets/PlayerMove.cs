e6ce27b27103d87767c1f61de05b815190935c2c
Pidgey <bastien.bouchet@3wa.io>
2022-09-09 16:39:55 +0200

    Player all animations + some collider tiles

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -15,16 +15,21 @@ public class PlayerMove : MonoBehaviour
     }
     private void Update()
     {
+        //run
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         float ud = Input.GetAxis("UpDiagonal");
         float dd = Input.GetAxis("DownDiagonal");
         move = new Vector2(h + ud + dd, v + Mathf.Abs(ud) - Mathf.Abs(dd));
+        //aniamtion roll
+        if (Input.GetButtonDown("Fire1")) animator.SetTrigger("SpaceDown");
+        if (Input.GetButtonUp("Fire1")) animator.SetTrigger("SpaceUp");
     }
     private void FixedUpdate()
     {
         Vector2 display = rb.velocity = move.normalized * speed * Time.deltaTime;        
         animator.SetFloat("x", display.normalized.x);
         animator.SetFloat("y", display.normalized.y);
+        if (Input.GetButton("Fire1")) rb.velocity *= 2;
     }
 }
